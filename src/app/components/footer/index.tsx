635fa43d3a7e8a5b1c807673e89b0bff5c8b6b1f
Alecor <alessio.corradini98@gmail.com>
2025-12-22 22:50:59 +0100

    Improvements

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,15 +1,14 @@
 "use client";
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <>
-    <div className={styles.footer} >
-      Diritti riservati a Simona Maleti
-    </div>    
+      <div className={styles.footer}>
+        Copyright (c) 2025 Simona Maleti. All Rights Reserved.
+      </div>
     </>
   );
 };
 
 export default Footer;
-
